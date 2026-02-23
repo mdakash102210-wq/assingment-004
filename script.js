73dbb2814dc023cc53a2428abe3932f4bcd582d0
mdakash102210-wq <mdaksh102210@gmail.com>
2026-02-23 01:46:11 -0800

    button queryselector diye dora dane

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ let noJob = document.querySelector(".noJob");
 let HtmlFakaArray = document.querySelector(".fala-Array");
 let rejectFakaArray = document.querySelector(".fala-Array-rejected");
 // console.log(noJob);
+let totalTracker = document.querySelector(".totalTracker");
+let interviewTracker = document.querySelector(".interviewTracker");
+let RejectedTracker = document.querySelector(".RejectedTracker");
+
+let totalJob = document.querySelector(".total-job");
+let jobMainContainer = document.querySelector(".job-main-container");
+
+totalTracker.innerText = jobMainContainer.children.length;
+totalJob.innerText = jobMainContainer.children.length;
+
+// button color
