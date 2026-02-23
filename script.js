5e3f1b9156dc4f46e26f687e5b3c136c67bae974
mdakash102210-wq <mdaksh102210@gmail.com>
2026-02-23 01:36:23 -0800

    java script start

---
+++ script.js
@@ -0,0 +1,7 @@
+let interviewlist = [];
+let rejectedlist = [];
+
+let noJob = document.querySelector(".noJob");
+let HtmlFakaArray = document.querySelector(".fala-Array");
+let rejectFakaArray = document.querySelector(".fala-Array-rejected");
+// console.log(noJob);
