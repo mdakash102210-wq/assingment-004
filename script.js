02aa506aae26dcb6d00f0a8e1f1ed177421ed35f
mdakash102210-wq <mdaksh102210@gmail.com>
2026-02-23 04:32:34 -0800

    buton color function dane

--- script.js
+++ script.js
@@ -16,3 +16,34 @@ totalTracker.innerText = jobMainContainer.children.length;
 totalJob.innerText = jobMainContainer.children.length;
 
 // button color
+
+function btnColorChange(id) {
+  let allBtn = document.querySelector("#btn001");
+  let interviewBtn = document.querySelector("#interview");
+  let rejectBtn = document.querySelector("#reject");
+
+  allBtn.classList.remove("bg-primary", "text-white");
+  interviewBtn.classList.remove("bg-primary", "text-white");
+  rejectBtn.classList.remove("bg-primary", "text-white");
+
+  document.getElementById(id).classList.add("bg-primary", "text-white");
+
+  // hidden section
+  jobMainContainer.classList.add("hidden");
+  HtmlFakaArray.classList.add("hidden");
+  rejectFakaArray.classList.add("hidden");
+  noJob.classList.add("hidden");
+
+  if (id === "btn001") {
+    jobMainContainer.classList.remove("hidden");
+    totalJob.innerText = jobMainContainer.children.length;
+  } else if (id === "interview") {
+    HtmlFakaArray.classList.remove("hidden");
+    totalJob.innerText = interviewlist.length;
+    interviewFunction();
+  } else if (id === "reject") {
+    rejectFakaArray.classList.remove("hidden");
+    totalJob.innerText = rejectedlist.length;
+    RejectedFunction();
+  }
+}
